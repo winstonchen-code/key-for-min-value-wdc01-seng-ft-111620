0ff61964081289208af4db183e3424dbc13484f4
Winston Chen <winstonchen.wm@gmail.com>
2020-11-04 21:23:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
  smallest_value = 0
- empty_hash = nil
+ empty_hash = {}
  name_hash.each do |name, value|
   if smallest_value == 0 || value < smallest_value
     smallest_value = value
